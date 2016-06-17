9a61d74f6caaeb6bb50411b3fb9c20ab56032b63
yyw <yyw_wzu@163.com>
2016-06-17 15:14:48 +0800

    twoCommit

---
+++ daogenerator/src/main/java/com/example/gankgenerator.java
@@ -0,0 +1,24 @@
+package com.example;
+
+import de.greenrobot.daogenerator.DaoGenerator;
+import de.greenrobot.daogenerator.Entity;
+import de.greenrobot.daogenerator.Schema;
+
+public class gankgenerator {
+    public static void main(String[] args) throws Exception{
+        Schema schema = new Schema(1,"com.yyw.abc.ganhuo");
+        addBean(schema);
+        new DaoGenerator().generateAll(schema,"../Ganhuo/app/src/main/java-gen");
+    }
+    public static void addBean(Schema schema){
+        Entity entity = schema.addEntity("Gank");
+        entity.addIdProperty().autoincrement();
+        entity.addStringProperty("sid").notNull();
+        entity.addStringProperty("url");
+        entity.addStringProperty("who");
+        entity.addStringProperty("type");
+        entity.addStringProperty("publishedAt");
+        entity.addStringProperty("desc");
+
+    }
+}
